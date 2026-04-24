49388234dc916a6f5dfa3b230c9346b58479a565
Nibir Alam <nibiralpha@gmail.com>
2026-04-24 20:12:21 +0600

    spider man animaion added

--- app/Components/HomeComponent/HomeComponent.tsx
+++ app/Components/HomeComponent/HomeComponent.tsx
@@ -1,7 +1,9 @@
-"use client"
+"use client";
 
 import React from "react";
 import style from "./Home.module.css";
+
+import "animate.css";
 
 export default function HomeComponent() {
   return (
@@ -10,7 +12,11 @@ export default function HomeComponent() {
         <div className={style.super_heroes}>
           <img className={style.super_heroes_img} src="/all-heroes.png" />
         </div>
-        <div className={style.spider_man}><img className={style.spider_man_img} src="/spider-man.png" /></div>
+        <div
+          className={`${style.spider_man} animate__animated animate__bounceInDown`}
+        >
+          <img className={style.spider_man_img} src="/spider-man.png" />
+        </div>
         <div className={style.text_area}>
           <div className={`${style.text} ${style.primary_font}`}>
             <h1>Create Your own Team of Superheroes</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,6 @@ export default function Home() {
   return (
     <>
       <HomeComponent />
-      {/* <div className="bg_img"> */}
-        {/* <div className="main">
-        <div className="super_heroes">
-          <img className="super_heroes_img" src="/all-heroes.png" />
-        </div>
-        <div className="spider_man"><img className="spider_man_img" src="/spider-man.png" /></div>
-        <div className="text_area">
-          <div className="text primary_font">
-            <h1>Create Your own Team of Superheroesas</h1>
-          </div>
-          <div className="enter_button">ENTER</div>
-        </div>
-      </div> */}
-      {/* </div> */}
     </>
   );
 }
