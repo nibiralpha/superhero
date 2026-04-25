3220907f69df2a4a381a119605798976238765a1
Nibir Alam <nibiralpha@gmail.com>
2026-04-25 16:10:12 +0600

    changing project layout

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeComponent from "./Components/HomeComponent/HomeComponent";
+
+export default function Home() {
+  return (
+    <>
+      <HomeComponent />
+    </>
+  );
+}
