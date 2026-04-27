af2c58901bd45319900538bb058cca9f145aa9a1
Nibir Alam <nibiralpha@gmail.com>
2026-04-27 21:43:03 +0600

    hero component added

--- src/app/Components/ComicsComponent/ComicsComponent.tsx
+++ src/app/Components/ComicsComponent/ComicsComponent.tsx
@@ -1,94 +1,30 @@
 "use client";
-
-import React, { useState } from "react";
-import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
-
-import { useRouter } from "next/navigation";
-import { Switch } from "antd";
 import style from "./Comics.module.css";
 
 import "animate.css";
 import { Col, Row } from "antd";
+import HeroesComponent from "../HeroesComponent/HeroesComponent";
 
 export default function ComicsComponent() {
-  const [showDetail, setShowDetail] = useState(false);
-  const router = useRouter();
+  // const [showDetail, setShowDetail] = useState(false);
+  // const router = useRouter();
 
-  const onChange = (checked) => {
-    console.log(`switch to ${checked}`);
-  };
-  const changeRoute = () => {
-    router.push("/details/11");
-  };
-
-  const showDetailCard = (status: boolean) => {
-    setShowDetail(status);
-  };
+ 
 
   return (
     <div className={style.layout}>
       <Row gutter={[24, 24]}>
         <Col xs={24} md={8} lg={6}>
-          <div
-            onMouseLeave={() => showDetailCard(false)}
-            className={style.img_container}
-          >
-            <img src={"/10060.jpg"} onClick={changeRoute}/>
-
-            <div
-              className={`${style.red_overlay} ${showDetail ? style.show : ""}`}
-            >
-              <div className={style.show_list}>
-                <div className={style.title}>A TEAM</div>
-                <div className={style.add_team}>
-                  <div className={style.text}>Add to team</div>
-                  <div className={style.switch}>
-                    <Switch
-                      checkedChildren={<CheckOutlined />}
-                      unCheckedChildren={<CloseOutlined />}
-                      defaultChecked
-                    />
-                  </div>
-                </div>
-                <div className={style.contents}>
-                  <div className={style.list}>Intelligence : 38</div>
-                  <div className={style.list}>strength : 47</div>
-                  <div className={style.list}>Speed : 17</div>
-                  <div className={style.list}>Durability : 36</div>
-                  <div className={style.list}>Power : 15</div>
-                  <div className={style.list}>Combat : 81</div>
-                </div>
-              </div>
-            </div>
-
-            <div className={style.content}>
-              {showDetail ? "" : <div className={style.name}>A-Bomb</div>}
-              <div className={style.team}>
-                {showDetail ? (
-                  <></>
-                ) : (
-                  <>
-                    <div className={style.text}>Add to team</div>
-                    <div className={style.switch}>
-                      <Switch
-                        checkedChildren={<CheckOutlined />}
-                        unCheckedChildren={<CloseOutlined />}
-                        defaultChecked
-                      />
-                    </div>
-                  </>
-                )}
-              </div>
-              <div className={style.rot_button}>
-                <img
-                  onMouseEnter={() => showDetailCard(true)}
-                  className={style.detail_icon}
-                  src={"/details.svg"}
-                  width="25px"
-                />
-              </div>
-            </div>
-          </div>
+          <HeroesComponent />
+        </Col>
+        <Col xs={24} md={8} lg={6}>
+          <HeroesComponent />
+        </Col>
+        <Col xs={24} md={8} lg={6}>
+          <HeroesComponent />
+        </Col>
+        <Col xs={24} md={8} lg={6}>
+          <HeroesComponent />
         </Col>
       </Row>
     </div>
--- src/app/Components/HeroesComponent/HeroesComponent.tsx
+++ src/app/Components/HeroesComponent/HeroesComponent.tsx
@@ -1,21 +1,86 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import style from "./Heroes.module.css";
 
 import "animate.css";
+import { Switch } from "antd";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 
 export default function HeroesComponent() {
+  const [showDetail, setShowDetail] = useState(false);
   const router = useRouter();
 
-  const changeRoute = () => {    
-    router.push("/list");
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+  };
+  const changeRoute = () => {
+    router.push("/details/11");
+  };
+
+  const showDetailCard = (status: boolean) => {
+    setShowDetail(status);
   };
 
   return (
-    <div className={style.heroes}>
-      
+    <div
+      onMouseLeave={() => showDetailCard(false)}
+      className={style.img_container}
+    >
+      <img src={"/10060.jpg"} onClick={changeRoute} />
+
+      <div className={`${style.red_overlay} ${showDetail ? style.show : ""}`}>
+        <div className={style.show_list}>
+          <div className={style.title}>A TEAM</div>
+          <div className={style.add_team}>
+            <div className={style.text}>Add to team</div>
+            <div className={style.switch}>
+              <Switch
+                checkedChildren={<CheckOutlined />}
+                unCheckedChildren={<CloseOutlined />}
+                defaultChecked
+              />
+            </div>
+          </div>
+          <div className={style.contents}>
+            <div className={style.list}>Intelligence : 38</div>
+            <div className={style.list}>strength : 47</div>
+            <div className={style.list}>Speed : 17</div>
+            <div className={style.list}>Durability : 36</div>
+            <div className={style.list}>Power : 15</div>
+            <div className={style.list}>Combat : 81</div>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.content}>
+        {showDetail ? "" : <div className={style.name}>A-Bomb</div>}
+        <div className={style.team}>
+          {showDetail ? (
+            <></>
+          ) : (
+            <>
+              <div className={style.text}>Add to team</div>
+              <div className={style.switch}>
+                <Switch
+                  checkedChildren={<CheckOutlined />}
+                  unCheckedChildren={<CloseOutlined />}
+                  defaultChecked
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className={style.rot_button}>
+          <img
+            onMouseEnter={() => showDetailCard(true)}
+            className={style.detail_icon}
+            src={"/details.svg"}
+            width="25px"
+          />
+        </div>
+      </div>
     </div>
   );
 }
