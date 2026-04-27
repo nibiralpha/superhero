5b56bea654b8fee2c0dfcd97843faa1f1a37fbbc
Nibir Alam <nibiralpha@gmail.com>
2026-04-27 21:25:31 +0600

    overlay teams section basic done

--- src/app/Components/ComicsComponent/ComicsComponent.tsx
+++ src/app/Components/ComicsComponent/ComicsComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 
 import { useRouter } from "next/navigation";
@@ -11,6 +11,7 @@ import "animate.css";
 import { Col, Row } from "antd";
 
 export default function ComicsComponent() {
+  const [showDetail, setShowDetail] = useState(false);
   const router = useRouter();
 
   const onChange = (checked) => {
@@ -20,110 +21,72 @@ export default function ComicsComponent() {
     router.push("/details/11");
   };
 
+  const showDetailCard = (status: boolean) => {
+    setShowDetail(status);
+  };
+
   return (
     <div className={style.layout}>
       <Row gutter={[24, 24]}>
         <Col xs={24} md={8} lg={6}>
-          <div onClick={changeRoute} className={style.img_container}>
+          <div
+            onMouseLeave={() => showDetailCard(false)}
+            onClick={changeRoute}
+            className={style.img_container}
+          >
             <img src={"/10060.jpg"} />
-            <div className={style.content}>
-              <div className={style.name}>A-Bomb</div>
-              <div className={style.team}>
-                <div className={style.text}>Add to team</div>
-                <div className={style.switch}>
-                  <Switch
-                    checkedChildren={<CheckOutlined />}
-                    unCheckedChildren={<CloseOutlined />}
-                    defaultChecked
-                  />
+
+            <div
+              className={`${style.red_overlay} ${showDetail ? style.show : ""}`}
+            >
+              <div className={style.show_list}>
+                <div className={style.title}>A TEAM</div>
+                <div className={style.add_team}>
+                  <div className={style.text}>Add to team</div>
+                  <div className={style.switch}>
+                    <Switch
+                      checkedChildren={<CheckOutlined />}
+                      unCheckedChildren={<CloseOutlined />}
+                      defaultChecked
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className={style.rot_button}>
-                <img src={"/details.svg"} width="25px" />
+                <div className={style.contents}>
+                  <div className={style.list}>Intelligence : 38</div>
+                  <div className={style.list}>strength : 47</div>
+                  <div className={style.list}>Speed : 17</div>
+                  <div className={style.list}>Durability : 36</div>
+                  <div className={style.list}>Power : 15</div>
+                  <div className={style.list}>Combat : 81</div>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
-        <Col xs={24} md={8} lg={6}>
-          <div className={style.img_container}>
-            <img src={"/10060.jpg"} />
+
             <div className={style.content}>
-              <div className={style.name}>A-Bomb</div>
+              {showDetail ? "" : <div className={style.name}>A-Bomb</div>}
               <div className={style.team}>
-                <div className={style.text}>Add to team</div>
-                <div className={style.switch}>
-                  <Switch
-                    checkedChildren={<CheckOutlined />}
-                    unCheckedChildren={<CloseOutlined />}
-                    defaultChecked
-                  />
-                </div>
+                {showDetail ? (
+                  <></>
+                ) : (
+                  <>
+                    <div className={style.text}>Add to team</div>
+                    <div className={style.switch}>
+                      <Switch
+                        checkedChildren={<CheckOutlined />}
+                        unCheckedChildren={<CloseOutlined />}
+                        defaultChecked
+                      />
+                    </div>
+                  </>
+                )}
               </div>
               <div className={style.rot_button}>
-                <img src={"/details.svg"} width="25px" />
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col xs={24} md={8} lg={6}>
-          <div className={style.img_container}>
-            <img src={"/10060.jpg"} />
-            <div className={style.content}>
-              <div className={style.name}>A-Bomb</div>
-              <div className={style.team}>
-                <div className={style.text}>Add to team</div>
-                <div className={style.switch}>
-                  <Switch
-                    checkedChildren={<CheckOutlined />}
-                    unCheckedChildren={<CloseOutlined />}
-                    defaultChecked
-                  />
-                </div>
-              </div>
-              <div className={style.rot_button}>
-                <img src={"/details.svg"} width="25px" />
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col xs={24} md={8} lg={6}>
-          <div className={style.img_container}>
-            <img src={"/10060.jpg"} />
-            <div className={style.content}>
-              <div className={style.name}>A-Bomb</div>
-              <div className={style.team}>
-                <div className={style.text}>Add to team</div>
-                <div className={style.switch}>
-                  <Switch
-                    checkedChildren={<CheckOutlined />}
-                    unCheckedChildren={<CloseOutlined />}
-                    defaultChecked
-                  />
-                </div>
-              </div>
-              <div className={style.rot_button}>
-                <img src={"/details.svg"} width="25px" />
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col xs={24} md={8} lg={6}>
-          <div className={style.img_container}>
-            <img src={"/10060.jpg"} />
-            <div className={style.content}>
-              <div className={style.name}>A-Bomb</div>
-              <div className={style.team}>
-                <div className={style.text}>Add to team</div>
-                <div className={style.switch}>
-                  <Switch
-                    checkedChildren={<CheckOutlined />}
-                    unCheckedChildren={<CloseOutlined />}
-                    defaultChecked
-                  />
-                </div>
-              </div>
-              <div className={style.rot_button}>
-                <img src={"/details.svg"} width="25px" />
+                <img
+                  onMouseEnter={() => showDetailCard(true)}
+                  className={style.detail_icon}
+                  src={"/details.svg"}
+                  width="25px"
+                />
               </div>
             </div>
           </div>
