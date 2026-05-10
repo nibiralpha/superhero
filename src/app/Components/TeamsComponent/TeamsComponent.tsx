bd9d3961b09fd4177ce1b631908ae6bb7f7f9e87
Nibir Alam <nibiralpha@gmail.com>
2026-05-10 19:34:36 +0600

    minor changes

--- src/app/Components/TeamsComponent/TeamsComponent.tsx
+++ src/app/Components/TeamsComponent/TeamsComponent.tsx
@@ -7,7 +7,7 @@ import HeroesComponent from "../HeroesComponent/HeroesComponent";
 import { useEffect, useState } from "react";
 
 export default function TeamsComponent() {
-  const [heroList, setHeroList] = useState([]);
+  const [heroList, setHeroList] = useState(null);
 
   useEffect(() => {
     reRednder();
@@ -18,10 +18,12 @@ export default function TeamsComponent() {
     setHeroList(heroList);
   };
 
+  if (heroList === null) return null;
+
   return (
     <div className={heroList.length === 0 ? style.alt_layout : style.layout}>
       <div>
-        {heroList.length === 0 && (
+        {heroList.length === 0 ? (
           <div className={style.no_team}>
             <div>
               You do not have any team members selected. Please make selections
@@ -29,9 +31,7 @@ export default function TeamsComponent() {
             </div>
             <div className={style.list_page}>Superheroes page</div>
           </div>
-        )}
-
-        {heroList.length > 0 && (
+        ) : (
           <Row gutter={[24, 24]}>
             {heroList.map((hero: any) => (
               <Col key={hero.id} sm={24} xs={24} md={8} lg={6}>
