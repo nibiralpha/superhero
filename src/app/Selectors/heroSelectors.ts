c675c3c86537baee0f43405cf39dfdbf3d73ba40
Nibir Alam <nibiralpha@gmail.com>
2026-05-12 22:14:22 +0600

    minor changes

--- src/app/Selectors/heroSelectors.ts
+++ src/app/Selectors/heroSelectors.ts
@@ -29,13 +29,21 @@ export const selectFilteredHeroes = createSelector(
       )
         return false;
 
-        //search with alignment
+      //search with alignment
       if (
         alignment &&
         hero.biography?.alignment?.toLowerCase().trim() !==
           alignment.toLowerCase().trim()
       )
         return false;
+
+      // // Powerstats (Numerical - assuming hero.powerstats object)
+      // // We check if the hero stat is greater than or equal to the filter value
+      // if (powerstate && hero.powerstats.power < powerstate) return false;
+      // if (intelligence && hero.powerstats.intelligence < intelligence) return false;
+      // if (speed && hero.powerstats.speed < speed) return false;
+      // if (power && hero.powerstats.power < power) return false;
+      // if (durability && hero.powerstats.durability < durability) return false;
 
       return true;
     });
